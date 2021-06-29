117b6e5f9722000d4187c4e490f065e5fdb536f5
Stewart Watkiss <stewart@penguintutor.com>
2021-06-29 23:22:04 +0100

    Start circuit demonstration

---
+++ democircuit1.py
@@ -0,0 +1,129 @@
+from pgzanimation import Slide, AnimLine
+import pygame, sys
+
+WIDTH = 1280
+HEIGHT = 720
+FRAMES = 1000
+TITLE = "Basic Electrical Circuit"
+
+# Save options
+# Continue to save frames when paused?
+SAVE_PAUSED = True
+# enable to show frame number on screen (not included in save)
+SHOW_FRAME = True
+# Exit after last frame?
+QUIT_END = False
+
+# Is save enabled (otherwise just display animation)
+save = True
+# frame is the animation frame number
+# when pause then save_frame continues to count to extend video length
+frame=0
+save_frame=0
+pause = False
+
+background_color = (255,255,255)
+background_image = ""
+
+slides = [
+    Slide((WIDTH,HEIGHT), TITLE)
+    ]
+
+# Can use shapes and/or shape_groups
+# shape groups are a dictionary of shape dictionaries
+# allows you to group items together to make changes to all simulataneously
+# often simpler to use shapes, but useful when you want to make changes to lots of objects
+# order is important for overlapping, shape_groups are written first (so get overwritten)
+# then shapes in order they appear in list
+# It's not possible to change order, but you can have multiple entries with the same image and
+# swap which is displayed as a way of positioning in the stack
+shape_groups = {
+    'wires': {
+        'top-start': AnimLine ((300,300), (900,300), (128,128,128), width=4)
+        },
+    'anim-wires': {
+        'top-start': AnimLine ((300,300), (900,300), (0,0,0), style="dashed", width=4, spacing=[5,10], dashanimate=0.2)
+        }
+    }
+shapes = {
+    }
+
+# Add any pauses to this
+# Pauses can also result from certain animations (eg. after each bullet)
+pause_frames = []
+
+# key frames (can just use number if preferred, but allows labels)
+kf = {
+    }
+
+# Show slide for entire animation
+slides[0].show_slide(0, FRAMES)
+
+
+def draw():
+    screen.clear()
+    screen.fill(background_color)
+    if (background_image != ""):
+        screen.blit(background_image, (0,0))
+
+    for this_slide in slides:
+        this_slide.draw()
+    for this_group in shape_groups:
+        for this_shape in shape_groups[this_group].values():
+            this_shape.draw()
+    for this_shape in shapes.values():
+        this_shape.draw()
+
+def update():
+    controls()
+    animate()
+    # Call update for all objects in case they need to animate
+    for this_slide in slides:
+        this_slide.update()
+    for this_group in shape_groups:
+        for this_shape in shape_groups[this_group].values():
+            this_shape.update()
+    for this_shape in shapes.values():
+        this_shape.update()
+
+
+# Place your animations here
+def animate():
+    pass
+
+def controls():
+    global frame, save_frame, pause
+    # Stop updating once end reached
+    if (frame > FRAMES):
+        if (QUIT_END):
+            pygame.quit()
+            sys.exit()
+        return
+        # Are we currently paused
+    if (pause == True):
+        if (SAVE_PAUSED):
+            save_frame += 1
+        return
+    # slides need to check for pause
+    for this_slide in slides:
+        if (this_slide.update(frame) == True): pause=True
+
+    frame += 1
+    save_frame += 1
+    print ("Frame is {}".format(frame))
+
+# Handle keys - when released
+def on_key_up (key, mod):
+    global pause
+    # quit on q / esc
+    if (key == keys.ESCAPE or key == keys.Q):
+        pygame.quit()
+        sys.exit()
+    # Space / p to pause / unpause
+    if (key == keys.SPACE or key == keys.P):
+        pause = not pause
+
+# Handle mouse up (to remove pause)
+def on_mouse_up (pos, button):
+    global pause
+    pause = False
