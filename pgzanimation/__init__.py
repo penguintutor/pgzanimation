f5f72ba26321b3269c74dd50948e068f51b5f597
Stewart Watkiss <stewart@penguintutor.com>
2021-07-05 23:02:57 +0100

    Update pgzanimation to PEP 8 and start update to PEP 257

--- pgzanimation/__init__.py
+++ pgzanimation/__init__.py
@@ -1,3 +1,16 @@
+""" PGZAnimation, a library for animations
+
+This expands upon Pygame Zero providing a simple way
+to create animations. It allows all of the Pygame Zero
+methods, but adds new methods that support keyframes
+and tweens.
+This needs to be run from your own code in Pygame Zero
+using pgzrun.
+Import the relevant classes that you need from pgzanimation.
+
+For more details see http://www.penguintutor.com/pgzanimation
+"""
+
 from .animactor import AnimActor
 from .animpolygon import AnimFilledPolygon, AnimPolygon
 from .animrect import AnimFilledRect, AnimRect
@@ -10,3 +23,5 @@ from .animmarqueeline import AnimMarqueeLine
 from .animvectors import get_dir_vector
 from .slide import Slide
 from .slidebullets import SlideBullets
+
+__version__ = '0.1.devel'
--- pgzanimation/pgzanimation.py
+++ pgzanimation/pgzanimation.py
@@ -3,22 +3,37 @@ import math
 
 # Library for creating animations in pygame zero which can be exported
 # as png image files and optionally converted to videos
-
 # For more details see http://www.penguintutor.com/programming/pgzanimation
 
 
-
 class PgzAnimation():
+    """ PgzAnimation parent class for animation classes
 
-    # start from top left and goes width across and height down
-    def __init__(self, color, anchor=('center', 'center'), hide=False, angle=0):
+    All the animation classes should be subclassed from this.
+    It provides basic functionality which is common across all
+    animation classes. All sub classes must provide the basic
+    functionality of the attributes, but can add any additional
+    one required by that class.
+    """
+
+    def __init__(
+            self,
+            color,
+            anchor=('center', 'center'),
+            hide=False,
+            angle=0):
+        """ init for all animation subclasses.
+
+        All the subclasses must call this during __init__ to
+        provide access to the Pygame Surface.
+        """
+
         self._color = color
 
         # angle in degrees
         self._angle = angle
         self.hide = hide
         self._anchor = [*anchor]
-
 
         # Get Pygame surface for draw()
         self._surface = pygame.display.get_surface()
@@ -42,7 +57,7 @@ class PgzAnimation():
     @angle.setter
     def angle(self, new_value):
         new_value %= 360
-        self.rotate (new_value)
+        self.rotate(new_value)
 
     # position - starts based on original
     @property
@@ -52,13 +67,13 @@ class PgzAnimation():
     # setting position moves immediately to new position
     # use a tuple for newpos
     @pos.setter
-    def pos(self, newpos):
+    def pos(self, new_pos):
         # Uses child move method
-        self.move (newpos)
+        self.move(new_pos)
 
-    # Updating anchor changes anchor (rotation point), without changing the _points
-    # The pos will change to the new anchor pos
-    # The rot_points needs to be recalculated to make them relative to the new anchor
+    # Updating anchor changes anchor (rotation point),
+    # the calc_pos() and _transform() methods are used to set position
+    # relative to the new anchor
     @property
     def anchor(self):
         return self._anchor
@@ -72,34 +87,49 @@ class PgzAnimation():
 
     # returns size of surface (screen size)
     def get_screen_size(self):
-        return (self._surface.get_size())
+        '''get_screen_size() - returns screen size in pixels (width, height)'''
+        return(self._surface.get_size())
 
-    # show and end_show are same as calling hide, but you need to give a start time of when to start showing or end showing the object
-    def show (self, start, current):
+    # show and end_show are same as calling hide, but you need to give
+    # a start time of when to start showing or end showing the object
+    def show(self, start, current):
+        ''' show(start, current) - unhides the instance
+
+        start is the frame to start showing the object
+        current is the current frame
+
+        This is the same as changing the .hide attribute when start == current
+        '''
         if (current == start):
             self.hide = False
 
-    def end_show (self, end, current):
+    def end_show(self, end, current):
+        ''' end_show(start, current) - hides the instance
+
+        start is the frame to stop showing the object
+        current is the current frame
+
+        This is the same as changing the .hide attribute when start == current
+        '''
         if (current == end):
             self.hide = True
 
-
     # rotate to an absolute from current position
-    # direction can be CCW / left (counter clock wise) or CW / right (Clockwise)
-    def rotate_tween (self, start, end, current, angle, direction="CCW"):
+    # direction can be CCW / left (counter clock wise)
+    # or CW / right (Clockwise)
+    def rotate_tween(self, start, end, current, angle, direction="CCW"):
         if (current < start or current > end):
             return
         if (current == start):
             self.rotate_start_angle = self._angle
         d_angle = (angle - self.rotate_start_angle) / (end-start)
-        #print ("Rotate from {} to {} - delta {}".format(self._angle, angle, d_angle))
         if ((direction == "CW" or direction == "right") and d_angle > 0):
             d_angle *= -1
         new_angle = self.rotate_start_angle + (d_angle * (current-start))
         self.rotate(new_angle)
 
     # rotate a relative amount
-    def rotate_rel_tween (self, start, end, current, angle, direction="CCW"):
+    def rotate_rel_tween(self, start, end, current, angle, direction="CCW"):
         if (current < start or current > end):
             return
         if (current == start):
@@ -110,7 +140,7 @@ class PgzAnimation():
         self.rotate(self._angle+rel_angle)
 
     # tweened movement to absolute position
-    def move_tween (self, start, end, current, goto):
+    def move_tween(self, start, end, current, goto):
         if (current < start or current > end):
             return
         if (current == start):
@@ -119,14 +149,14 @@ class PgzAnimation():
         dx = (goto[0] - self.move_start_pos[0]) / (end-start)
         dy = (goto[1] - self.move_start_pos[1]) / (end-start)
 
-        newpos = [0,0]
+        newpos = [0, 0]
         # Add delta to start position
         newpos[0] = self.move_start_pos[0] + dx * (current - start)
         newpos[1] = self.move_start_pos[1] + dy * (current - start)
-        self.move (newpos)
+        self.move(newpos)
 
     # tweened movement to relative position
-    def move_rel_tween (self, start, end, current, delta):
+    def move_rel_tween(self, start, end, current, delta):
         if (current < start or current > end):
             return
         if (current == start):
@@ -135,21 +165,19 @@ class PgzAnimation():
         dx = delta[0] / (end-start)
         dy = delta[1] / (end-start)
 
-        newpos = [0,0]
+        newpos = [0, 0]
         # Add delta to current position
         newpos[0] = self._pos[0] + dx
         newpos[1] = self._pos[1] + dy
-        self.move (newpos)
-
+        self.move(newpos)
 
     # Print test with information about the shape
     # Used for debugging
     # Overwrite in individual classes for more detailed information
-    def print_info (self):
-        print ("Shape: Unknown")
-        print ("Position: "+str(self._pos))
-        print ("Implement in individual class for more information")
-
+    def print_info(self):
+        print("Shape: Unknown")
+        print("Position: "+str(self._pos))
+        print("Implement in individual class for more information")
 
     # update can take an optional argument of current_frame
     # This is not used on all classes, but allows consistancy with slides
@@ -159,4 +187,3 @@ class PgzAnimation():
     # eg. dashed line - this will still be updated even if animation paused
     def update(self, current_frame=-1):
         return False
-        
