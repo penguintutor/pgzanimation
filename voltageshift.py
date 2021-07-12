5d91c47e697f93056057998f4c51325f99a721d9
Stewart Watkiss <stewart@penguintutor.com>
2021-07-12 22:21:18 +0100

    Extend voltage shift demonstration

--- voltageshift.py
+++ voltageshift.py
@@ -1,9 +1,11 @@
-from pgzanimation import Slide, AnimLine, AnimMarqueeLine, AnimActor, AnimCircle, AnimText
+from pgzanimation import Slide
+from pgzanimation import AnimLine, AnimMarqueeLine, AnimActor, AnimCircle, AnimFilledCircle, AnimText
 import pygame, sys
 
 WIDTH = 1280
 HEIGHT = 720
-FRAMES = 50
+FRAMES = 1000
+FRAME_DELAY = 1
 TITLE = "Inverting buffer"
 
 # Save options
@@ -23,6 +25,7 @@ save_files = "/home/stewart/test-animations/animation-{0:05d}.png"
 # when pause then save_frame continues to count to extend video length
 frame=0
 save_frame=0
+frame_delay = 0
 pause = False
 
 background_color = (255, 255, 255)
@@ -40,8 +43,8 @@ rl_bottom = (713, 320)
 rl_pos = (714, 275)
 rl_top = (714, 229)
 plus_start = (432, 180)
-out_start = (714, 344)
-out_end = (770, 344)
+output_start = (714, 344)
+output_end = (810, 344)
 
 
 slides = [
@@ -60,41 +63,53 @@ shape_groups = {
     'wires-stage1': {
         'input': AnimMarqueeLine (
                 (input_start[0]+5, input_start[1]), (input_start[0] + 62, input_start[1]),
-                (255, 165, 0), (0, 0, 0),
+                "orange", "black",
                 width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
         'tomosfet': AnimMarqueeLine (
                 input_mosfet, (input_mosfet[0] + 77, input_start[1]),
-                (255, 165, 0), (0, 0, 0),
+                "orange", "black",
                 width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
         'toground':  AnimMarqueeLine (
                 source_mosfet, (source_mosfet[0], ground_start[1]),
-                (40,40,180), (0, 0, 0),
-                width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
-        'plustorl':  AnimMarqueeLine (
-                (rl_top[0], plus_start[1]), rl_top,
-                (180,40,40), (0, 0, 0),
-                width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
-        'rltodrain':  AnimMarqueeLine (
-                rl_bottom, drain_mosfet,
-                (180,40,40), (0, 0, 0),
+                "blue", "grey",
                 width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
         },
     'fixedwires' : {
         'plus':  AnimLine (
                 plus_start, (plus_start[0] + 400, plus_start[1]),
-                (180,40,40), (0, 0, 0),
-                width=4),
+                "red", width=4),
         'ground':  AnimLine (ground_start, (ground_start[0] + 400, ground_start[1]),
-                (40,40,180), (0, 0, 0),
-                width=4),
+                "blue", width=4),
+        'plustext' : AnimText ("+5V", (plus_start[0] - 30, plus_start[1] - 30), "black", fontsize=40),
+        'groundtext' : AnimText ("Gnd", (ground_start[0] - 30, ground_start[1] + 10), "black", fontsize=40),
+
         }
     }
 shapes = {
-    'terminal1' : AnimCircle ((input_start),10, (0,0,0), width=4),
-    'inputtext' : AnimText ("Input", (input_start[0] - 50, input_start[1] - 50), (0,0,0), fontsize=40),
+    'plustorl':  AnimMarqueeLine (
+            (rl_top[0], plus_start[1]), rl_top,
+            "red", "grey",
+            width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
+    'rltodrain':  AnimMarqueeLine (
+            rl_bottom, drain_mosfet,
+            "red", "grey",
+            width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
+    'outwire':  AnimMarqueeLine (
+            output_start, (output_end[0] - 7, output_end[1]),
+            "red", "grey",
+            width=4, animwidth=2, style="dashed", spacing=[10,10], animatedisplay=False, dashanimate=1),
+    'terminal1' : AnimCircle ((input_start),10, "black", width=4),
+    'terminal2' : AnimCircle ((output_end),10, "black", width=4),
+    'inputtext' : AnimText ("Input", (input_start[0] - 80, input_start[1] - 50), "black", fontsize=40),
+    'inputvoltage' : AnimText ("0V", (input_start[0] - 15, input_start[1] + 5), "black", fontsize=40, anchor=("right", "center")),
+    'outputtext' : AnimText ("Output", (output_end[0] + 10, output_end[1] - 40), "black", fontsize=40),
+    'outputvoltage' : AnimText ("5V", (output_end[0] + 70, output_end[1] + 5), "black", fontsize=40),
     'mosfet1' : AnimActor ("mosfet", mosfet_pos),
+    'mosfettext' : AnimText ("MOSFET", (720, 400), "black", fontsize=40),
+    'mosfetstatustext' : AnimText ("Off", (720, 430), "black", fontsize=40),
     'rb' :  AnimActor ("resistorh", rb_pos),
     'rl' :  AnimActor ("resistorv", rl_pos),
+    'output_join' : AnimFilledCircle (output_start, 9, "black")
     }
 
 # Add any pauses to this
@@ -104,10 +119,9 @@ pause_frames = []
 # key frames (can just use number if preferred, but allows labels)
 kf = {
     'start': 0,
-    'start-close': 5,
-    'close': 10,
-    'start-open': 40,
-    'open': 45
+    'start-out': 100,
+    'turn-on': 300,
+    'turn-off': 750
     }
 
 # Set any initial states
@@ -117,16 +131,34 @@ slides[0].show_slide(0, FRAMES)
 
 # Place your animations here
 def animate():
-    #shapes['switchlevel'].rotate_tween (kf['start-close'], kf['close'], frame, 0, direction="CW")
-    if (frame == kf['close']):
-        for this_group in shape_groups:
-            for this_shape in shape_groups[this_group].values():
-                this_shape.animatedisplay=True
-    #shapes['switchlevel'].rotate_tween (kf['start-open'], kf['open'], frame, 30, direction="CCW")
-    if (frame == kf['start-open']):
-        for this_group in shape_groups:
-            for this_shape in shape_groups[this_group].values():
-                this_shape.animatedisplay=False
+    if (frame == kf['start-out']):
+        shapes['plustorl'].animatedisplay=True
+        shapes['rltodrain'].animatedisplay=True
+        shapes['outwire'].animatedisplay=True
+
+    if (frame == kf['turn-on']):
+        this_group = shape_groups["wires-stage1"]
+        shapes['inputvoltage'].text = "3.3V"
+        shapes['mosfetstatustext'].text = "On"
+        shapes['outputvoltage'].text = "0V"
+        shapes['outwire'].reverse()
+        shapes['outwire'].color = "blue"
+        shapes['rltodrain'].color = "blue"
+        for this_shape in this_group.values():
+            this_shape.animatedisplay=True
+
+    if (frame == kf['turn-off']):
+        this_group = shape_groups["wires-stage1"]
+        shapes['inputvoltage'].text = "0V"
+        shapes['mosfetstatustext'].text = "Off"
+        shapes['outputvoltage'].text = "5V"
+        shapes['outwire'].reverse()
+        shapes['outwire'].color = "blue"
+        shapes['rltodrain'].color = "blue"
+        for this_shape in this_group.values():
+            this_shape.animatedisplay=True
+
+
 
 def draw():
     screen.clear()
@@ -153,6 +185,14 @@ def draw():
         screen.draw.text("Mouse "+str(pygame.mouse.get_pos()), (20, 60), color=(50, 50, 250), fontsize=60)
 
 def update():
+    global frame_delay
+    # delay updates based on frame delay
+    if (FRAME_DELAY > 0):
+        if (frame_delay < FRAME_DELAY):
+            frame_delay += 1
+            return
+        else:
+            frame_delay = 0
     controls()
     animate()
     # Call update for all objects in case they need to animate
@@ -163,7 +203,6 @@ def update():
             this_shape.update()
     for this_shape in shapes.values():
         this_shape.update()
-
 
 
 def controls():
@@ -185,7 +224,7 @@ def controls():
 
     frame += 1
     save_frame += 1
-    print ("Frame is {}".format(frame))
+    #print ("Frame is {}".format(frame))
 
 # Handle keys - when released
 def on_key_up (key, mod):
